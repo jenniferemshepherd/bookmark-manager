10d4c012341a5275dcd7cf5cfdd36fe55c19a3ae
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-23 20:10:24 +0000

    now cleans the database so tests will not give false positives

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'capybara/rspec'
+require 'database_cleaner'
 
 require './app/models/favourite'
 require './app/app'
@@ -6,11 +7,29 @@ require './app/app'
 Capybara.app = BookmarkManager
 
 RSpec.configure do |config|
+  # Everything in this block runs once before all the tests run
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
